3acd588524059c4bd3992745200ee93d61c72869
RishabhCompanyHub <rishabh@companyhub.com>
2025-05-23 17:17:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,4 @@ server: {
   host: true,
   strictPort: true,
   port: 5173,
-  cors: true,
-  origin: 'https://dev--bdd169c65a224ce987907120141eb50b.aitester.io',
-  hmr: {
-    host: 'dev--bdd169c65a224ce987907120141eb50b.aitester.io',
-    port: 443,
-    protocol: 'wss'
-  }
 }})
